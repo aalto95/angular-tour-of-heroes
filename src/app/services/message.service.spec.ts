39d623b98ad25b6dc74e4b089b79d2e834248374
aaltoo <stanportableuse@gmail.com>
2023-02-25 19:21:31 +0900

    Add tests for message service

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -13,4 +13,14 @@ describe('MessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should clear messages', () => {
+    service.clear();
+    expect(service.messages).toHaveSize(0);
+  });
+
+  it('should push message', () => {
+    service.add('message');
+    expect(service.messages).toHaveSize(1);
+  });
 });
