3d4321ba23b42601c7e998196847ef9919b9dbbd
aaltoo <stanportableuse@gmail.com>
2023-02-25 19:14:09 +0900

    Create mock of service in dashboard test

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,33 @@
+import { Hero } from './../../interfaces/hero';
+import { HeroService } from './../../services/hero.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let heroServiceStub: Partial<HeroService> = {
+    getHeroes: () => {
+      const heroes: Hero[] = [
+        {
+          id: 0,
+          name: 'Batman',
+        },
+        {
+          id: 1,
+          name: 'Flash',
+        },
+      ];
+      return of(heroes);
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
+      providers: [{ provide: HeroService, useValue: heroServiceStub }],
     }).compileComponents();
   });
 
